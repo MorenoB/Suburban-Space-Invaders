f745466559000fd09b3320918bc24a5626818e53
MorenoB <moreno_bralts@hotmail.com>
2014-09-19 22:30:04 +0200

    Imported testmap model

---
+++ src/mygame/Map.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+import com.jme3.scene.Mesh;
+
+/**
+ *
+ * @author Moreno
+ */
+public class Map {
+    public Map()
+    {
+    
+    }
+}
